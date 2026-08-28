d1b126e565cb70c071efc70c1d2e0bfaf78e5ca2
xtsinghang <2395058425@qq.com>
2026-08-28 23:21:09 +0800

    new file square_motion_node.cpp

---
+++ elfin_ws/src/cos_shape/src/square_motion_node.cpp
@@ -0,0 +1,27 @@
+
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <cmath>
+#include <future>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <vector>
+
+#include <rclcpp/rclcpp.hpp>
+#include <geometry_msgs/msg/point.hpp>
+#include <geometry_msgs/msg/pose.hpp>
+#include <std_msgs/msg/bool.hpp>
+#include <std_msgs/msg/float64.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <trajectory_msgs/msg/joint_trajectory.hpp>
+
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/robot_trajectory/robot_trajectory.h>
+#include <moveit/trajectory_processing/iterative_time_parameterization.h>
+
+namespace
+{
+    
+}
